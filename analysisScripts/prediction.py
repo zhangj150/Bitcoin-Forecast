7cdfdd1b73127581a287edb74d55bdba093e428b
zhangj150 <zhangj@gatech.edu>
2017-04-18 17:20:29 -0400

    fixed overfitting

--- analysisScripts/prediction.py
+++ analysisScripts/prediction.py
@@ -15,28 +15,39 @@ dataset = dataset['Weighted Price'].replace(to_replace=0, method='ffill') #repla
 # autocorrelation_plot(dataset) #258 is good starting point for AR model
 # plt.show()
 
-
-#partial
-
-#model = ARIMA(dataset, order=(258, 1, 0))
-#model_fit = model.fit(disp=0)
-#print (model_fit.summary())
 X = dataset.values
 size = int(len(X) * 0.66)
 train, test = X[0:size], X[size:len(X)]
 history = [x for x in train]
-predictions = list()
+predictions = []
+#$newTest = []
 print("got to for loop")
-for i in range(len(test)):
-    model = ARIMA(history, order=(10,2,0))
+k = 5 #forecast the next k time steps
+for i in range(0, len(test)-k+1, 1):
+    model = ARIMA(history, order=(1,1,0))
     model_fit = model.fit(disp=0)
-    output = model_fit.forecast()
-    predicted = output[0]
-    predictions.append(predicted)
-    actual = test[i]
-    history.append(actual)
-    print('predicted=%f, expected=%f' % (predicted, actual))
-error = mean_squared_error(test, predictions)
+    output,stderr,conf = model_fit.forecast(steps=k)
+    #print(output)
+    #predicted1, predicted2 = output[0], output[1]
+    #print (predicted1, predicted2)
+    if (i%k == 0):
+        #print(output)
+        
+        #print(predicted)
+        #predictions.append(predicted1)
+        #predictions.append(predicted2)
+        for f in range(k):
+            predictions.append(output[f])
+            # actual1, actual2 = test[i], test[i+1]
+            # history.append(actual1)
+            # history.append(actual2)
+            history.append(test[i+f])
+            #newTest.append(test[i])
+            #print('predicted=%f, expected=%f' % (predicted1, actual1))
+            #print('predicted=%f, expected=%f' % (predicted2, actual2))
+            print('predicted=%f, expected=%f' % (output[f], test[i+f]))
+print (len(test), len(predictions))
+error = mean_squared_error(test[0:len(predictions)], predictions)
 print("MSE: " + str(error))
 
 plt.plot(test)
